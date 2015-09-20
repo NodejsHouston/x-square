1616206fccc77097594ce4fa3c36cec860882bd4
august1073 <augustmoney81@gmail.com>
2015-09-20 10:13:28 -0500

    Added some routes and templates
    Added login and create route. Also created the templates for them. There
    is nothing in the templates yet aside from <h1> to let you know if it
    has loaded.

--- app/router.js
+++ app/router.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
 Router.map(function() {
+  this.route('index', {path: '/'});
+  this.route('login', {path: '/login'});
+  this.route('create', {path: '/create'});
 });
 
 export default Router;
