d8cce4445dd11f14721628935282097014d81e06
august <augustmoney81@gmail.com>
2015-09-29 19:47:22 -0500

    added templates

--- app/router.js
+++ app/router.js
@@ -6,7 +6,6 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('index', {path: '/'});
   this.route('login', {path: '/login'});
   this.route('create', {path: '/create'});
 });
